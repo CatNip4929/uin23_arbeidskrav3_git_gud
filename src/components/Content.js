c19631f8a8f027c34d94299f79e24a5aa29ceb7b
CatNip4929 <111730636+CatNip4929@users.noreply.github.com>
2023-02-13 13:55:01 +0100

    content component
    started on content component
    and title component

---
+++ src/components/Content.js
@@ -0,0 +1,13 @@
+import resources from '../resources.json'
+
+export default funciton Content () {
+    return (
+        <main>
+            <article>
+             <h2>
+             
+             </h2>
+            </article>
+        </main>
+    )
+}
