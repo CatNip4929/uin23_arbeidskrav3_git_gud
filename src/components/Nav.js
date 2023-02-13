c352d23fb54eaa4b5e95a0d2dcada1954c0b1022
fmelvold <78048522+fmelvold@users.noreply.github.com>
2023-02-13 16:07:20 +0100

    mappet category fra json

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,18 @@
 import Title from "./Title";
+import resources from '../resources.json'
+import Resources from "./Resources";
 
-export default function Nav({ cat }) {
+export default function Nav({cat}) {
   return (
     <nav>
       <button>
         <Title />
+        {resources.map((resources) =>{
+             return(
+                <Resources cat={resources.category} />
+                )
+                } 
+            )}
       </button>
     </nav>
   );
