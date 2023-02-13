add07223dc5d4735631ea10115425298b64e423b
snilseen <111301891+snilseen@users.noreply.github.com>
2023-02-13 16:21:54 +0100

    mappet navfunksjonen
    Mappet navfunksjonen inn i indivduelle buttons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,18 @@
 import Title from "./Title";
-import resources from '../resources.json'
+import resources from "../resources.json";
 import Resources from "./Resources";
 
-export default function Nav({cat}) {
+export default function Nav({ cat }) {
   return (
     <nav>
-      <button>
-        <Title />
-        {resources.map((resources) =>{
-             return(
-                <Resources cat={resources.category} />
-                )
-                } 
-            )}
-      </button>
+      <Title />
+      {resources.map((resources) => {
+        return (
+          <button className="nav-btns" onClick="activate">
+            <Resources cat={resources.category} />
+          </button>
+        );
+      })}
     </nav>
   );
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,3 @@
-export default function Title({cat}){
-    return(
-        <h2>{cat}</h2>
-    )
-    
+export default function Title({ cat }) {
+  return <h2>{cat}</h2>;
 }
