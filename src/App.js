c34bd4aa3f9de2425abaf513e085c4f8c707a603
fmelvold <78048522+fmelvold@users.noreply.github.com>
2023-02-23 00:57:23 +0100

    små endringer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MyComponent from "./components/MyComponent";
 import Content from "./components/Content";
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
     <main>
     <MyComponent />
       <Routes>
-          <Route path="html" element={<Content title="HTML" />} />
-          <Route path="css" element={<Content title="CSS" />} />
-          <Route path="javascript" element={<Content title="Javascript" />} />
-          <Route path="reactpage" element={<Content title="React" />} />
-          <Route path="sanity" element={<Content title="Sanity" />} />
+          <Route path="/html" element={<Content title="HTML" />} />
+          <Route path="/css" element={<Content title="CSS" />} />
+          <Route path="/javascript" element={<Content title="Javascript" />} />
+          <Route path="/reactpage" element={<Content title="React" />} />
+          <Route path="/sanity" element={<Content title="Sanity" />} />
       </Routes>
       </main>
     </BrowserRouter>
   );
 }
-
-export default App;
