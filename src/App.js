65821ac9535ce801fe3dfc469bfca5ddd7094571
CatNip4929 <111730636+CatNip4929@users.noreply.github.com>
2023-02-24 23:01:16 +0100

    Shhtylin
    importert main.scss til index. js for å ha muligheten til å endre css'en etter routinga. Lagt til article className på, artikkelen rundt routinga av resources.js.  className nav på nav bar. Begynt på stylingen i sass, vurdere å lage variabler til gjenbruk av farger i sassen, om en skal lage en egen fil _color.scss

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import filer from './resources/filer';
 import Resouces from './components/Resouces';
+import Header from './components/Header';
 
 export default function App() {
   return (
+    
     <Router>
-      <nav>
+      <Header />
+      <nav className='nav'>
         {filer.map((item, index) => (
           <Link key={index} to={item.category}>
             {item.category}
           </Link>
         ))}
       </nav>
+      <article className='article'>
       <Routes>
         {filer.map((item, index) => (
           <Route key={index} path={item.category} element={<Resouces category={item.category} />} />
         ))}
       </Routes>
+      </article>
     </Router>
   );
 }
