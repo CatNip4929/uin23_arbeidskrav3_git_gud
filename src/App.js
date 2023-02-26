0bba09c0a4202ce51619d2ab9c723908b319aa69
snilseen <nilzen@live.com>
2023-02-26 22:42:42 +0100

    Router App
    Routa map funksjonen i Resources med path til riktig stedet. Tror dette skal funke og vi er så og si done.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import filer from './resources/filer';
-import Resouces from './components/Resouces';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import filer from "./resources/filer";
+import Resouces from "./components/Resouces";
 
 export default function App() {
   return (
-    
-  )
+    <Router>
+      <nav>
+        {filer.map((item, index) => (
+          <Link key={index} to={item.category}>
+            {item.category}
+          </Link>
+        ))}
+      </nav>
+      <Routes>
+        {filer.map((item, index) => (
+          <Route
+            key={index}
+            path={item.category}
+            element={<Resouces category={item.category} />}
+          />
+        ))}
+      </Routes>
+    </Router>
+  );
 }
